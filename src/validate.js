a0654caae09a81fc1dd0a8f1162ecfcb5aaaae45
frieswithsalsa <romansedin@hotmail.com>
2024-11-29 11:42:38 +0200

    Url validation schema

--- src/validate.js
+++ src/validate.js
@@ -1,16 +1,5 @@
 import * as yup from yup;
 
-const schema = yup.object().shape({
-    adress: yup.string()
-        .url('Введите правильный URL')
-        .required('Адресс обязателен')
-});
-
-const validateAdress = async (adress) => {
-    try {
-        await schema.validate({ adress });
-        console.log('Адресс валиден')
-    } catch (error) {
-        console.log(error.erros)
-    }
-}
+const schema = yup.object.shape({
+    input: yup.string().url().required(),
+})
