67c5059e2c77d1fa608479364ca52df78c17b4e1
frieswithsalsa <romansedin@hotmail.com>
2025-01-24 18:38:28 +0200

    Changed ext

---
+++ webpack.config.mjs
@@ -0,0 +1,48 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import process  from 'process';
+
+const mode = process.env.NODE_ENV || 'development';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+  },
+
+  watchOptions: {
+    poll: 1000,
+  },
+  devServer: {
+    host: 'localhost',
+    port: 9000,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+};
+
+export default {
+  ...config,
+  mode,
+};
