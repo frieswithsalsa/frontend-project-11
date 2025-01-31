d4a033ea3146522b83d45b8fb73f42b75edcc410
frieswithsalsa <romansedin@hotmail.com>
2025-01-31 09:23:29 +0200

    Fixes

--- src/rss.js
+++ src/rss.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { get } from "lodash";
 
 const fetchRss = async (url) => {
     const proxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent('url')}`;
 
-    const response = await axios get(proxyUrl);
+    const response = await axios.get(proxyUrl);
 
     return response.data.contents;
 }
