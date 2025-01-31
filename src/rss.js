71d534f43b6066bc2938fcc52692ae70bedece9e
frieswithsalsa <romansedin@hotmail.com>
2025-01-31 12:15:26 +0200

    rss created

--- src/rss.js
+++ src/rss.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const fetchRss = async (url) => {
-    const proxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent('url')}`;
-
+export const fetchRss = async (url) => {
+    const proxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`;
     const response = await axios.get(proxyUrl);
-
     return response.data.contents;
 }
