107844c78ff57f6c7af8687ef318157ce07eeebc
frieswithsalsa <romansedin@hotmail.com>
2025-02-04 08:22:15 +0200

    Cache disabled

--- src/rss.js
+++ src/rss.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchRss = async (url) => {
-    const proxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`;
+    const proxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`;
     const response = await axios.get(proxyUrl);
     return response.data.contents;
-}
+};
